4a3534f6f2f1e3cac521b32055a8ae5c9cf880c9
Paul Chen <paulchenpmc@gmail.com>
2018-03-14 10:34:58 -0600

    almost have create account fixed with new method stuff.

--- ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -1,7 +1,7 @@
 ﻿// To be completed by students in milestone 2
 
 function validateLoginForm() {
-    console.log("\n\n\nI'm in the validation checker thing!!!\n\n\n");
+    //console.log("\n\n\nI'm in the validation checker thing!!!\n\n\n");
     if (loginForm.usernameData.value === "") {
         return false;
     }
@@ -11,37 +11,9 @@ function validateLoginForm() {
     return true;
 }
 
-//function validateCAUsernameForm() {
-//    if (asIsEchoForm.asIsText.value === "") {
-//        return false;
-//    }
-//    return true;
-//}
-
-//function validateCAPasswordForm() {
-//    if (asIsEchoForm.asIsText.value === "") {
-//        return false;
-//    }
-//    return true;
-//}
-
-//function validateCAAddressForm() {
-//    if (asIsEchoForm.asIsText.value === "") {
-//        return false;
-//    }
-//    return true;
-//}
-
-//function validateCAEmailForm() {
-//    if (asIsEchoForm.asIsText.value === "") {
-//        return false;
-//    }
-//    return true;
-//}
-
-//function validateCAPhoneForm() {
-//    if (asIsEchoForm.asIsText.value === "") {
-//        return false;
-//    }
-//    return true;
-//}
+function validateCreateAccountForm() {
+    if (createAccountForm.usernameData.value === "") {
+        return false;
+    }
+    return true;
+}
