0b05580b47b10ea38f5eda2a48d83e69d8b65284
Paul Chen <paulchenpmc@gmail.com>
2018-03-14 16:01:33 -0600

    Create Account fields now have error catching for email and phone number fields

--- ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -1,7 +1,6 @@
 ﻿// To be completed by students in milestone 2
 
 function validateLoginForm() {
-    //console.log("\n\n\nI'm in the validation checker thing!!!\n\n\n");
     if (loginForm.usernameData.value === "") {
         return false;
     }
@@ -24,7 +23,15 @@ function validateCreateAccountForm() {
     if (createAccountForm.emailData.value === "") {
         return false;
     }
+    var emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRe.test(String(createAccountForm.emailData.value).toLowerCase())) {
+        return false;
+    }
     if (createAccountForm.phoneData.value === "") {
+        return false;
+    }
+    var phoneRe = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    if (!phoneRe.test(String(createAccountForm.phoneData.value).toLowerCase())) {
         return false;
     }
     return true;
