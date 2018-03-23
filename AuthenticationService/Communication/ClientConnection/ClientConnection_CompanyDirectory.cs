d7e5b3d1a47b4afa4cf63e8bb4953e4b4c09a99f
Chantal Chung <chantal.chung@live.com>
2018-03-22 20:09:34 -0600

    Added functionality to Search

--- AuthenticationService/Communication/ClientConnection/ClientConnection_CompanyDirectory.cs
+++ AuthenticationService/Communication/ClientConnection/ClientConnection_CompanyDirectory.cs
@@ -1,6 +1,11 @@
-﻿using Messages.ServiceBusRequest;
+﻿using Messages.NServiceBus.Commands;
+using Messages.NServiceBus.Events;
+using Messages.ServiceBusRequest;
 using Messages.ServiceBusRequest.CompanyDirectory;
 using Messages.ServiceBusRequest.CompanyDirectory.Requests;
+
+using NServiceBus;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,26 +23,31 @@ namespace AuthenticationService.Communication
                 case (CompanyDirectoryRequest.CompanySearch):
                     return companySearch((CompanySearchRequest)request);
                 case (CompanyDirectoryRequest.GetCompanyInfo):
-                    return getCompany((GetCompanyInfoRequest)request);
+                    return getCompanyInfo((GetCompanyInfoRequest)request);
                 default:
-                    return new ServiceBusResponse(false, "Error: Invalid Request. Request reveived was:" + request.requestType.ToString());
-
+                    return new ServiceBusResponse(false, "Error: Invalid Request. Request received was:" + request.requestType.ToString());
             }
         }
 
         private ServiceBusResponse companySearch(CompanySearchRequest request)
         {
-            return null;
+            if (authenticated == false)
+            {
+                return new ServiceBusResponse(false, "Error: You must be logged in to use the search companies functionality.");
+            }
+
+            string searchKey = request.searchDeliminator; //how do i send this to search method in CompanyListingController
+
+            // This class indicates to the request function where 
+            SendOptions sendOptions = new SendOptions();
+            sendOptions.SetDestination("CompanyListings");
+
+            return requestingEndpoint.Request<ServiceBusResponse>(request, sendOptions).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
-        private ServiceBusResponse getCompany(GetCompanyInfoRequest request)
+        private ServiceBusResponse getCompanyInfo(GetCompanyInfoRequest request)
         {
-            return null;
+            return new ServiceBusResponse(true, "Getting Company Info");
         }
-    }
-
-    partial class ClientConnection
-    {
-
     }
 }
