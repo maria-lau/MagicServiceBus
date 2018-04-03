f85f4f6d15038fb503226b7c295075fcc736d2d2
maria-lau <m.marialau@gmail.com>
2018-04-03 13:26:46 -0600

    work in progress.

---
+++ ClientApplicationMVC/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ClientApplicationMVC.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // If you define a “public void” function within this class, 
+        // you will be able to call it through javascript on the client side
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
--- ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -11,27 +11,14 @@ function validateLoginForm() {
 }
 
 function validateCreateAccountForm() {
-    if (createAccountForm.usernameData.value === "") {
-        return false;
-    }
-    if (createAccountForm.passwordData.value === "") {
-        return false;
-    }
-    if (createAccountForm.addressData.value === "") {
-        return false;
-    }
-    if (createAccountForm.emailData.value === "") {
-        return false;
-    }
     var emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!emailRe.test(String(createAccountForm.emailData.value).toLowerCase())) {
-        return false;
-    }
-    if (createAccountForm.phoneData.value === "") {
+        alert("Please enter a valid e-mail.");
         return false;
     }
     var phoneRe = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
     if (!phoneRe.test(String(createAccountForm.phoneData.value).toLowerCase())) {
+        alert("Please enter a valid phone number with no spaces or dashes.");
         return false;
     }
     return true;
