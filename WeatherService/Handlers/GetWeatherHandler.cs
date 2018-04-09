88297947bde4fb4c67f14b43ddd226ef14527658
maria-lau <m.marialau@gmail.com>
2018-04-09 02:44:27 -0600

    Final Touch Ups For Milestone 6

--- WeatherService/Handlers/GetWeatherHandler.cs
+++ WeatherService/Handlers/GetWeatherHandler.cs
@@ -26,7 +26,7 @@ namespace WeatherService.Handlers
             string apikey4 = "ClB7bdIhjEr2kAAA928rGbRQfKCbHdOS";
             string city = message.city;
             string province = message.province;
-            string apiurl = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + apikey + "&q=" + city + "%2C" +
+            string apiurl = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + apikey2 + "&q=" + city + "%2C" +
                                 province + "%2CCanada&language=en-ca&details=false&offset=1 HTTP/1.1";
 
             HttpResponseMessage httpresponse = client.GetAsync(apiurl).Result;
@@ -43,7 +43,7 @@ namespace WeatherService.Handlers
                 string[] tokens = citysearchresponse.Split('"');
                 string locationkey = tokens[5];
                 // get weather info with location key
-                apiurl = "http://dataservice.accuweather.com/currentconditions/v1/" + locationkey + "?apikey=" + apikey + "&language=en-ca&details=true";
+                apiurl = "http://dataservice.accuweather.com/currentconditions/v1/" + locationkey + "?apikey=" + apikey2 + "&language=en-ca&details=true";
                 HttpResponseMessage httpresponse2 = client.GetAsync(apiurl).Result;
                 HttpContent content2 = httpresponse2.Content;
                 JavaScriptSerializer js = new JavaScriptSerializer();
